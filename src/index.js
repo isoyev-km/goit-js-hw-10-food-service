61fd4896cab72d5fdb8e572c297898eb7ff7a634
Kamil <60015567+isoyev-km@users.noreply.github.com>
2020-11-02 10:06:16 +0200

    hw10 done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-import value from "./js/a";
-import "./css/styles.scss";
+import "./styles.css";
+import template from "./menu-template.hbs";
+import menuJSON from "./menu.json"
 
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
 
-const add = (a, b) => a + b;
-add(2, 4);
+const toggleRef = document.querySelector('.theme-switch__toggle');
+const localStorageTheme = localStorage.getItem('theme');
 
-console.log(value);
+toggleRef.addEventListener('change', toggleChange);
+
+function toggleChange() {
+    if (!toggleRef.checked) {
+        addLightTheme();
+    } else {
+        addDarkTheme();
+    }
+}
+
+function addLightTheme() {
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function addDarkTheme() {
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+}
+
+function LocalStorageHandler() {
+    if (localStorageTheme === Theme.DARK) {
+        document.body.classList.toggle(Theme.DARK);
+        toggleRef.checked = true;
+    } else {
+        document.body.classList.toggle(Theme.LIGHT);
+        toggleRef.checked = false;
+    }
+}
+
+LocalStorageHandler()
+
+const createMarkup = template(menuJSON);
+
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', createMarkup);
--- src/js/a.js
+++
@@ -1,3 +0,0 @@
-const a = 5;
-
-export default a;
